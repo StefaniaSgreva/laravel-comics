61624095024694cf8db31de38c98445cffc7e8fe
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-15 20:32:20 +0100

    added top-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,55 @@
 <footer>
     <div class="top-footer">
-        
+        <div class="container  relative">
+            <nav>
+                <div class="R-nav">
+                        <ul>
+                            <h4>Dc Comics</h4>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <ul>
+                            <h4>Shop</h4>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop Dc Collectibles</a></li>
+                        </ul>
+                </div>
+                <div class="next-nav">
+                    <ul>
+                        <h4>Dc</h4>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                    <ul>
+                        <h4>Sites</h4>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="disclaimer">All Site Content TM ans © 2020 DC Entertainment, unless otherwise <a href="#" class="footer-link">noted here</a>. All rights reserved.
+            <br><a href="#" class="footer-link">Coockies Settings</a></div>
+            <div class="dc-logo-footer">
+                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="DC Footer Logo">
+            </div>
+        </div>
     </div>
 
     <div class="bottom-footer">
