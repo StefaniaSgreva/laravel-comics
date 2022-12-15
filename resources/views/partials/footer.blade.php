3663d76249bb4824afb3783933c4280b982ff048
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-15 19:26:29 +0100

    added bottom-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,19 @@
 <footer>
-    FOOTER
+    <div class="top-footer">
+        
+    </div>
+
+    <div class="bottom-footer">
+        <div class="container d-flex justify-content-between">
+            <button>Sign-up Now!</button>
+            <div class="icon-links">
+                <span>Follow Us</span>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook link"></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter link"></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="YouTube link"></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="Pinterest link"></a>
+                <a href="#"><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="Maps link"></a>
+            </div>
+        </div>
+    </div>
 </footer>
